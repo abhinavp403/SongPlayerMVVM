8d73ba6c5dab08f89a4ca87a33590c22924bc56e
abhinavp403 <abhinavp403@gmail.com>
2023-10-02 20:01:27 -0400

    Removed compose, downgraded lib versions

--- app/src/main/java/com/example/songplayermvvm/di/AppModule.kt
+++ app/src/main/java/com/example/songplayermvvm/di/AppModule.kt
@@ -11,11 +11,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ApplicationComponent::class)
 class AppModule {
 
     @Singleton
